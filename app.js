5caf15d9e8f93e70c3c6e9d1aa90d3bca982ce5d
dnlslcd <danielsalcedoq@gmail.com>
2024-09-09 19:04:42 +0200

    Iteración 2 - empezada

--- app.js
+++ app.js
@@ -27,25 +27,43 @@ app.set("view engine", "ejs");
 app.get("/", async (req, res) => {
     //res.render("index");
 
-    // 1. hacer consulta a la bbdd y traer 10 pelis, ordenarlas por fecha de lanzamiento
+    // Iteración 1
+    // 1.1. hacer consulta a la bbdd y traer 10 pelis, ordenarlas por fecha de lanzamiento
     // de forma decreciente. Usar console.log para ver si realmente se recuperan los documentos.
 
     const movies = database.collection("movies");
-    const query = {};
+
+    // Iteración 2:
+    // 2.1. obtener query string, de momento, solo del parámetro title
+    const { title } = req.query; // { title } --> operador de desetructuración
+    
+    // 2.2. si la string trae valor, actualizar el objeto query con el filtro
+
+    // definir un objeto que va a contener la query a la bbdd
+    let query = {};
+
+    // si el usuario quiere buscar por título, añado esta información a la query
+    if (title) {
+        query = {
+            ...query, // ...(operador spread) --> copia los mismos campos que se tenía hasta ahora
+            title: new RegExp(title) // añadimos el campo title
+        }    
+    }
+
+
     const options = {};
     const cursor = movies.find(query).sort({year: -1}).limit(10);
 
     // recuperar las pelis y transformarlas en array:
     const documents = await cursor.toArray();
-    console.log(documents);
     
-     // 2. pasar a la vista los documentos recuperados
+     // 1.2. pasar a la vista los documentos recuperados
     res.render("index", {
         documents
     });
-     // 3. en el ejs, iterar por cada documento y, para cada uno, mostrar el título, imagen y año de lanzamiento.
+     // 1.3. en el ejs, iterar por cada documento y, para cada uno, mostrar el título, imagen y año de lanzamiento.
 
-
+    
 
 
 
